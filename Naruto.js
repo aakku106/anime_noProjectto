f5bc1d59886576e9a4f388e773c94cffc4471981
Aakku <155184299+Adarasha@users.noreply.github.com>
2024-02-09 19:44:37 +0545

    hide navigation-text
    mainly change in js and css to hide navatation text until opened image is clicked

--- Naruto.js
+++ Naruto.js
@@ -4,10 +4,12 @@ function toggleMenu() {
   var menuIcon = document.getElementById("menu-icon");
   var menuBtnClose = document.getElementById("menu-btn-close");
   var openedImage = document.getElementById("opened-image");
+  var navigationText = document.getElementById("navigation-text"); // Get the navigation text element
   var body = document.body;
 
   openedImage.classList.toggle("active"); // Toggle the 'active' class
   body.classList.toggle("active"); // Toggle the 'active' class on the body
+  navigationText.classList.toggle("active"); // Toggle the 'active' class on the navigation text
 
   if (menuIcon.style.display !== "none") {
     // If menu icon is visible, hide it and show close menu button
@@ -15,6 +17,7 @@ function toggleMenu() {
     menuBtnClose.style.display = "block";
     openedImage.src = "https://naruto-official.com/common/header/menu.png";
     openedImage.style.display = "block";
+    navigationText.style.display = "block"; // Display the navigation text
     return;
   }
 
@@ -22,4 +25,5 @@ function toggleMenu() {
   menuIcon.style.display = "block";
   menuBtnClose.style.display = "none";
   openedImage.style.display = "none";
+  navigationText.style.display = "none"; // Hide the navigation text
 }
