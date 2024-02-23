4dd5394de866a67202c1e07caf3ccc37a68da955
Adarasha Gaihre <155184299+aakku106@users.noreply.github.com>
2024-02-23 13:59:32 +0545

    Fixed nav blink
    Chabged several css styles and fixed the blinking of art in nav bar

--- content.js
+++ content.js
@@ -4,8 +4,8 @@ function blinkNavbarArtLink() {
   // Add a class to initiate the blinking effect
   navArtLink.classList.add("blink");
 
-  // Remove the class after a delay (e.g., 500ms)
+ 
   setTimeout(function () {
     navArtLink.classList.remove("blink");
-  }, 5050);
+  }, 5000);
 }
